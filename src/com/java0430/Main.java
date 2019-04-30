cf358caefc4e84bd2e93b0720e36ff48034edc6b
GD3 <GD3@DESKTOP-UBOJVCP>
2019-04-30 17:45:31 +0900

    JSP 셋팅 0430

---
+++ src/com/java0430/Main.java
@@ -0,0 +1,27 @@
+package com.java0430;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Main")
+public class Main extends HttpServlet { //  http://localhost:8080/201905/Main
+	private static final long serialVersionUID = 1L;
+    public Main() {
+
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+	}
+
+}
